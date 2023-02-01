ddca8b208534221520b5bfa44036c14c920d0830
Rolando <rolandoantoniohe@ufps.edu.co>
2023-02-01 16:28:32 -0500

    Copia de proyecto anterior
    se agrega toda la informacion de las tareas

---
+++ TodoListSofka/DTO/ToDoCreateDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListSofka.DTO
+{
+    public class ToDoCreateDTO
+    {
+        [Required] public string Title { get; set; } = null!;
+        [Required] public string Description { get; set; } = null!;
+        [Required] public string Responsible { get; set; } = null!;
+        [Required] public string Priority { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+
+        public ToDoCreateDTO(string title, string description, string responsible, string priority,
+            bool isCompleted)
+        {
+            Title = title;
+            Description = description;
+            Responsible = responsible;
+            Priority = priority;
+            IsCompleted = isCompleted;
+        }
+
+        public ToDoCreateDTO() { }
+    }
+}
---
+++ TodoListSofka/Data/ToDoAPIDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TodoListSofka.Model;
+
+namespace TodoListSofka.Data
+{
+    public class ToDoAPIDbContext : DbContext
+    {
+        public DbSet<TodoItem> Tareas { get; set; }
+
+        public ToDoAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Deleted &&
+                e.Metadata.GetProperties().Any(x => x.Name == "State")))
+            {
+                item.State = EntityState.Unchanged;
+                item.CurrentValues["State"] = false;
+            }
+
+            return base.SaveChanges();
+        }
+    
+    }
+}
