bb3c9ecb3aa585c3b3e9653caec3ff61a82874a1
jmgaviriam <jmgaviriam@gmail.com>
2023-02-02 12:03:34 -0500

    feat: SQL + Entity Framework (Data Base First)

---
+++ TodoListSofka/Models/Todoitem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListSofka.Models;
+
+public partial class Todoitem
+{
+    public Guid Id { get; set; }
+
+    public Guid IdCalendar { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Responsible { get; set; } = null!;
+
+    public bool IsCompleted { get; set; }
+
+    public bool State { get; set; }
+
+    public virtual Calendar? IdCalendarNavigation { get; set; }
+}
+
+/*
+ Scaffold-DbContext "Server=LAPTOP-UVJ9P68E\MSSQLSERVER01;Database=calendardb;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+ */
