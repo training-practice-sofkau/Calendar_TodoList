480576184ab8c6247094bad63f0ee17d941eebb3
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-02-01 19:18:16 -0500

    Data mejora

---
+++ TodoListSofka/Model/FechaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListSofka.Model;
+
+public partial class FechaModel
+{
+    public Guid Id { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public int? Dia { get; set; }
+
+    public int? Mes { get; set; }
+
+    public int? Año { get; set; }
+
+    public bool State { get; set; }
+
+    public Guid IdEventos { get; set; }
+    public FechaModel(Guid id, DateTime fecha, int dia, int mes,
+            int año, bool state)
+    {
+        Id = id;
+        Fecha = fecha;
+        Dia = dia;
+        Mes = mes;
+        Año = año;
+        State = state;
+    }
+
+    public FechaModel() { }
+}
---
+++ TodoListSofka/Model/TareaModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace TodoListSofka.Model;
+
+public partial class TareaModel
+{
+    public Guid Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public bool State { get; set; }
+
+    public virtual ICollection<FechaModel> Fechas { get; } = new List<FechaModel>();
+    public TareaModel(Guid id, String nombre, String descripcion, bool state)
+    {
+        Id = id;
+        Nombre = nombre;
+        Descripcion = descripcion;
+        State = state;
+    }
+    public TareaModel() { }
+
+}
