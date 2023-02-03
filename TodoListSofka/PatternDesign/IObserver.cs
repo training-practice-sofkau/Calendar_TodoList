1ccf469c38b3532f32535acced924280ca637c3f
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-03 01:50:28 -0500

    Adding Observable Pattern Desingn
    For notify to calendar subscriptors

---
+++ TodoListSofka/PatternDesign/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using TodoListSofka.Model;
+
+namespace TodoListSofka.PatternDesign
+{
+    public interface IObserver
+    {
+        public void Subscribe(Calendar calendar);
+        public void Unsubscribe(Calendar calendar);
+        public void Update(Calendar calendar);
+    }
+}
---
+++ TodoListSofka/PatternDesign/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TodoListSofka.Model;
+
+namespace TodoListSofka.PatternDesign
+{
+    public class User : IObserver
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public User(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public User()
+        {
+        }
+        public void Subscribe(Calendar calendar)
+        {
+            calendar._usersList.Add(this);
+        }
+
+        public void Unsubscribe(Calendar calendar)
+        {
+            calendar._usersList.Remove(this);
+        }
+
+        public void Update(Calendar calendar)
+        {
+            foreach (var user in calendar._usersList)
+            {
+                Console.WriteLine($"Usuario: {user.Name}, Se abrió la agenda el día: {calendar.IndexDay}");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
