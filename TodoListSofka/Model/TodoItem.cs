98f05dc2b66efed847ccc6bece59ef223dd319d8
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-03 01:45:56 -0500

    Adding ToDoItem model
    From DatabaseFirst implementation

--- TodoListSofka/Model/TodoItem.cs
+++ TodoListSofka/Model/TodoItem.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TodoListSofka.Model;
 
-public class ToDoItem
+public partial class ToDoItem
 {
     public Guid ItemId { get; set; }
     [Required]
@@ -17,15 +15,10 @@ public class ToDoItem
     public bool IsCompleted { get; set; }
     [Required]
     public bool State { get; set; }
-    public ToDoItem(Guid id, string title, string description, string responsible,
-            bool isCompleted, bool state)
-    {
-        ItemId = id;
-        Title = title;
-        Description = description;
-        Responsible = responsible;
-        IsCompleted = isCompleted;
-        State = state;
-    }
-    public ToDoItem() { }
+    [Required]
+    [Range(1, 28,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    public int? IndexDay { get; set; }
+
+    public virtual Calendar? IndexDayNavigation { get; set; }
 }
