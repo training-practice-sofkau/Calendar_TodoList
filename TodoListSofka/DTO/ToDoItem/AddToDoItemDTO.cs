22f5c82a70cf006dbcb844ac9d31a4d0c1b8feeb
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-03 01:47:43 -0500

    Adding ToDoItem DTO's
    For the client view

---
+++ TodoListSofka/DTO/ToDoItem/AddToDoItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListSofka.DTO.ToDoItem
+{
+    public class AddToDoItemDTO
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public string Responsible { get; set; } = null!;
+    }
+}
