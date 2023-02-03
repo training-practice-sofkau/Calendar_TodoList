df3eef1c20525153b0fe162fabb7ad2b890b283a
Kevin Baquero <baquerochavarro@gmail.com>
2023-02-03 01:47:30 -0500

    Adding Calendar DTO's
    For the client view

---
+++ TodoListSofka/DTO/Calendar/GetCalendarDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TodoListSofka.DTO.ToDoItem;
+
+namespace TodoListSofka.DTO.Calendar
+{
+    public class GetCalendarDTO
+    {
+        [Required]
+        public int IndexDay { get; set; }
+    }
+}
